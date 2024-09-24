25fb197878e09278edf4ca221ac3e06f5063c4bd
Emmanoel <emmanoel_95@hotmail.com>
2024-09-24 16:09:02 -0300

    build: base project

---
+++ src/main/java/com/ironhorse/IronHorseApplication.java
@@ -0,0 +1,13 @@
+package com.ironhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronHorseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IronHorseApplication.class, args);
+    }
+
+}
