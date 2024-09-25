71cdfbb599b55c024254ac978d6f7462d1fe66e2
Emmanoel <emmanoel_95@hotmail.com>
2024-09-24 21:57:06 -0300

    feat(UserController): add endpoint to create user

---
+++ src/main/java/com/ironhorse/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.ironhorse.controller;
+
+import com.ironhorse.dto.UserDto;
+import com.ironhorse.dto.UserResponseDto;
+import org.springframework.http.ResponseEntity;
+
+public interface UserController {
+    ResponseEntity<UserResponseDto> save(UserDto userDto);
+}
---
+++ src/main/java/com/ironhorse/controller/impl/UserControllerImpl.java
@@ -0,0 +1,28 @@
+package com.ironhorse.controller.impl;
+
+import com.ironhorse.controller.UserController;
+import com.ironhorse.dto.UserDto;
+import com.ironhorse.dto.UserResponseDto;
+import com.ironhorse.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserControllerImpl implements UserController {
+    private final UserService userService;
+
+    public UserControllerImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<UserResponseDto> save(@RequestBody @Valid UserDto userDto) {
+        UserResponseDto userResponseDto = this.userService.save(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDto);
+    }
+}
