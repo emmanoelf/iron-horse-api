82bc0cf1e0a6f92d1e1ab07acfc29bc0f63406a3
Emmanoel <emmanoel_95@hotmail.com>
2024-09-27 17:53:03 -0300

    feat(GlobalExceptionHandler): add exception for invalid data in dtos

--- src/main/java/com/ironhorse/dto/ProblemType.java
+++ src/main/java/com/ironhorse/dto/ProblemType.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-    ENTITY_NOT_FOUND("/entidade-nao-encontrada", "Entidade não encontrada");
+    ENTITY_NOT_FOUND("/entidade-nao-encontrada", "Entidade não encontrada"),
+    INVALID_DATA("/dados-invalidos", "Dados inválidos");
 
     private String title;
     private String uri;
--- src/main/java/com/ironhorse/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ironhorse/exception/GlobalExceptionHandler.java
@@ -1,14 +1,29 @@
 package com.ironhorse.exception;
 
 import com.ironhorse.dto.ProblemDetail;
+import com.ironhorse.dto.ProblemObject;
 import com.ironhorse.dto.ProblemType;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    public GlobalExceptionHandler(@Qualifier("messageSource") MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @ExceptionHandler(EntityNotFound.class)
     protected ResponseEntity<ProblemDetail> handleEntityNotFound(EntityNotFound ex) {
@@ -21,6 +36,34 @@ public class GlobalExceptionHandler {
         ).build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ProblemDetail> handleValidations(MethodArgumentNotValidException ex){
+        ProblemType problemType = ProblemType.INVALID_DATA;
+        String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+
+        List<ProblemObject> problemObjects = ex.getBindingResult().getAllErrors().stream()
+                .map(objectError -> {
+                    String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
+                    String name = objectError.getObjectName();
+
+                    if(objectError instanceof FieldError){
+                        name = ((FieldError) objectError).getField();
+                    }
+
+                    return ProblemObject.builder()
+                            .name(name)
+                            .userMessage(message)
+                            .build();
+                })
+                .collect(Collectors.toList());
+
+        ProblemDetail problemDetail = createProblemDetailBuilder(HttpStatus.BAD_REQUEST, problemType, detail)
+                .problemObjects(problemObjects)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
     }
 
     private ProblemDetail.ProblemDetailBuilder createProblemDetailBuilder(HttpStatus status, ProblemType problemType, String detail){
