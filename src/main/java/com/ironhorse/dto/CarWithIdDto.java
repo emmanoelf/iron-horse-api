1e6eefeed4dc3797e01de4ff2b0428bb6dbcd79b
Emmanoel <emmanoel_95@hotmail.com>
2024-10-09 23:18:15 -0300

    refactor(CarWithIdDto): change class to record

--- src/main/java/com/ironhorse/dto/CarWithIdDto.java
+++ src/main/java/com/ironhorse/dto/CarWithIdDto.java
@@ -3,10 +3,9 @@ package com.ironhorse.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public class CarWithIdDto {
+public record CarWithIdDto (
+        @NotNull @NotBlank String brand,
+        @NotNull @NotBlank String model,
+        @NotNull Long manufactureYear
+){}
 
-            @NotNull @NotBlank String brand,
-            @NotNull @NotBlank String model,
-            @NotNull Long manufactureYear
-    }
-}
