73c533610a1f6d971f304a4e8ac140891a95a782
Evairlf <evairleitefeldmann@gmail.com>
2024-10-13 21:47:56 -0300

    feat(ReviewController): add endpoint all-car-reviews

--- src/main/java/com/ironhorse/controller/ReviewController.java
+++ src/main/java/com/ironhorse/controller/ReviewController.java
@@ -1,4 +1,12 @@
 package com.ironhorse.controller;
 
+import com.ironhorse.dto.ReviewDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewController {
+
+    ResponseEntity<List<ReviewDto>> getReviewListById(Long carId);
 }
--- src/main/java/com/ironhorse/controller/impl/ReviewControllerImpl.java
+++ src/main/java/com/ironhorse/controller/impl/ReviewControllerImpl.java
@@ -1,26 +1,30 @@
 package com.ironhorse.controller.impl;
 
 import com.ironhorse.controller.ReviewController;
+import com.ironhorse.dto.CarResponseDto;
 import com.ironhorse.dto.ReviewDto;
+import com.ironhorse.model.Car;
+import com.ironhorse.service.CarService;
 import com.ironhorse.service.ReviewService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
-@RequestMapping("v1/car_overview")
+@RequestMapping("/v1/reviews")
 public class ReviewControllerImpl implements ReviewController {
 
     private final ReviewService reviewService;
 
-    @Autowired
+
     public ReviewControllerImpl(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
+
 
     @PostMapping
     public ResponseEntity<ReviewDto> createReview(@RequestBody ReviewDto reviewDto) {
@@ -34,11 +38,19 @@ public class ReviewControllerImpl implements ReviewController {
         return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getReviewById(@PathVariable Long id) {
-        ReviewDto reviewDto = this.reviewService.getReviewById(id);
+    @GetMapping("/{carId}")
+    public ResponseEntity<Object> getReviewById(@PathVariable Long carId) {
+        ReviewDto reviewDto = this.reviewService.getReviewById(carId);
         return ResponseEntity.status(HttpStatus.OK).body(reviewDto);
     }
+
+
+    @GetMapping("/all-car-reviews/{carId}")
+    public ResponseEntity<List<ReviewDto>> getReviewListById(@PathVariable Long carId){
+        List<ReviewDto> reviewDtos = this.reviewService.getReviewByCarId(carId);
+        return ResponseEntity.status(HttpStatus.OK).body(reviewDtos);
+    }
+
 
     @PutMapping("/{id}")
     public ResponseEntity<ReviewDto> updateReview(@PathVariable Long id, @RequestBody ReviewDto reviewDto) {
@@ -51,4 +63,5 @@ public class ReviewControllerImpl implements ReviewController {
         reviewService.deleteReview(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
 }
