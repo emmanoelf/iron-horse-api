d8e96cb9c6d7f5a93d3887d8c78750b6f1ddf61d
Emmanoel <emmanoel_95@hotmail.com>
2024-10-26 15:19:32 -0300

    refactor(UserInfo): remove userId from URI to Jwt Token

--- src/main/java/com/ironhorse/controller/impl/UserInfoControllerImpl.java
+++ src/main/java/com/ironhorse/controller/impl/UserInfoControllerImpl.java
@@ -4,6 +4,7 @@ import com.ironhorse.controller.UserInfoController;
 import com.ironhorse.dto.UserInfoCreateDto;
 import com.ironhorse.dto.UserInfoDto;
 import com.ironhorse.dto.UserInfoResponseDto;
+import com.ironhorse.service.AuthenticatedService;
 import com.ironhorse.service.UserInfoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -15,23 +16,27 @@ import org.springframework.web.bind.annotation.*;
 public class UserInfoControllerImpl implements UserInfoController {
 
     private final UserInfoService userInfoService;
+    private final AuthenticatedService authenticatedService;
 
-    public UserInfoControllerImpl(UserInfoService userInfoService) {
+    public UserInfoControllerImpl(UserInfoService userInfoService, AuthenticatedService authenticatedService) {
         this.userInfoService = userInfoService;
+        this.authenticatedService = authenticatedService;
     }
 
     @Override
-    @PostMapping("/{userId}")
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<UserInfoResponseDto> save(@RequestBody @Valid UserInfoCreateDto userInfoCreateDto, @PathVariable Long userId) {
+    public ResponseEntity<UserInfoResponseDto> save(@RequestBody @Valid UserInfoCreateDto userInfoCreateDto) {
+        Long userId = this.authenticatedService.getCurrentUserId();
         UserInfoResponseDto userInfoResponseDto = this.userInfoService.save(userInfoCreateDto, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(userInfoResponseDto);
     }
 
     @Override
-    @GetMapping("/{userId}")
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<UserInfoResponseDto> findByUserId(@PathVariable Long userId) {
+    public ResponseEntity<UserInfoResponseDto> findByUserId() {
+        Long userId = this.authenticatedService.getCurrentUserId();
         UserInfoResponseDto userInfoResponseDto = this.userInfoService.findByUserId(userId);
         return ResponseEntity.status(HttpStatus.OK).body(userInfoResponseDto);
     }
@@ -44,9 +49,9 @@ public class UserInfoControllerImpl implements UserInfoController {
     }
 
     @Override
-    @PutMapping("/{userId}")
-    public ResponseEntity<UserInfoResponseDto> update(@RequestBody @Valid UserInfoDto userInfoDto,
-                                                      @PathVariable Long userId) {
+    @PutMapping()
+    public ResponseEntity<UserInfoResponseDto> update(@RequestBody @Valid UserInfoDto userInfoDto) {
+        Long userId = this.authenticatedService.getCurrentUserId();
         UserInfoResponseDto userInfoResponseDto = this.userInfoService.update(userInfoDto, userId);
         return ResponseEntity.status(HttpStatus.OK).body(userInfoResponseDto);
     }
