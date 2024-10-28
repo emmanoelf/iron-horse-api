924ab5111f368cdb8c11ee4fe2ca6e15b80e38f4
Emmanoel <emmanoel_95@hotmail.com>
2024-10-28 16:53:38 -0300

    refactor(User): add mapping rentals to model user

--- src/main/java/com/ironhorse/model/User.java
+++ src/main/java/com/ironhorse/model/User.java
@@ -45,6 +45,9 @@ public class User {
     @Column(nullable = false)
     private UserRole role;
 
+    @OneToMany(mappedBy = "user")
+    private List<Rental> rentals;
+
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime created_at;
