8aac7d3ca9968abef72f8a11b844ddf25f16391d
Evairlf <evairleitefeldmann@gmail.com>
2024-11-08 20:47:17 -0300

    refactor: CarFeaturesMapper added new function toUpdate

--- src/main/java/com/ironhorse/mapper/CarFeaturesMapper.java
+++ src/main/java/com/ironhorse/mapper/CarFeaturesMapper.java
@@ -1,6 +1,7 @@
 package com.ironhorse.mapper;
 
 import com.ironhorse.dto.CarFeaturesDto;
+import com.ironhorse.dto.CarFeaturesUpdateDto;
 import com.ironhorse.dto.CarInfoConsentsDto;
 import com.ironhorse.model.CarFeatures;
 import com.ironhorse.model.CarInfo;
@@ -53,6 +54,53 @@ public class CarFeaturesMapper {
 
     }
 
+    public static CarFeatures toUpdateModel(CarFeaturesUpdateDto carFeaturesUpdateDto) {
+        return CarFeatures.builder()
+                .isSmokersAccepted(carFeaturesUpdateDto.smokersAccepted())
+                .tagPike(carFeaturesUpdateDto.tagPike())
+                .spareTire(carFeaturesUpdateDto.spareTire())
+                .electricWindowsAndLocks(carFeaturesUpdateDto.electricWindowsAndLocks())
+                .wheelWrench(carFeaturesUpdateDto.wheelWrench())
+                .jack(carFeaturesUpdateDto.jack())
+                .triangle(carFeaturesUpdateDto.triangle())
+                .isTagActivated(carFeaturesUpdateDto.tagActivated())
+                .alarm(carFeaturesUpdateDto.alarm())
+                .insulfilm(carFeaturesUpdateDto.insulfilm())
+                .multimedia(carFeaturesUpdateDto.multimedia())
+                .airConditioner(carFeaturesUpdateDto.airConditioner())
+                .fireExtinguisher(carFeaturesUpdateDto.fireExtinguisher())
+                .antiTheftSecret(carFeaturesUpdateDto.antiTheftSecret())
+                .isDocsUptoDate(carFeaturesUpdateDto.isDocsUptoDate())
+                .isFinesBelongToTheOffender(carFeaturesUpdateDto.isFinesBelongToTheOffender())
+                .isVeicleModified(carFeaturesUpdateDto.isVeicleModified())
+                .isTrueInformation(carFeaturesUpdateDto.isTrueInformation())
+                .build();
+    }
+
+    public static CarFeaturesUpdateDto toUpdateDto(CarFeatures carFeatures) {
+        CarFeaturesUpdateDto carFeaturesUpdateDto = new CarFeaturesUpdateDto(
+                carFeatures.isVeicleModified(),
+                carFeatures.isTrueInformation(),
+                carFeatures.isInsulfilm(),
+                carFeatures.isTagPike(),
+                carFeatures.isAntiTheftSecret(),
+                carFeatures.isMultimedia(),
+                carFeatures.isAirConditioner(),
+                carFeatures.isElectricWindowsAndLocks(),
+                carFeatures.isTriangle(),
+                carFeatures.isJack(),
+                carFeatures.isWheelWrench(),
+                carFeatures.isSpareTire(),
+                carFeatures.isFireExtinguisher(),
+                carFeatures.isAlarm(),
+                carFeatures.isSmokersAccepted(),
+                carFeatures.isTagActivated(),
+                carFeatures.isFinesBelongToTheOffender(),
+                carFeatures.isDocsUptoDate()
+        ); return carFeaturesUpdateDto;
+
+    }
+
     public static CarFeatures toPartialModel(CarInfoConsentsDto dto) {
         return CarFeatures.builder()
                 .isDocsUptoDate(dto.docsUptoDate())
@@ -64,7 +112,7 @@ public class CarFeaturesMapper {
                 .build();
     }
 
-    public static CarInfoConsentsDto toPartialDto(CarFeatures carFeatures){
+    public static CarInfoConsentsDto toPartialDto(CarFeatures carFeatures) {
         return new CarInfoConsentsDto(
                 carFeatures.isVeicleModified(),
                 carFeatures.isTrueInformation(),
