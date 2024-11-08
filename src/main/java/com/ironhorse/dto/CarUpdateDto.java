da0347808d2083fe19e7e0cc59d57e6901f6c36c
Evairlf <evairleitefeldmann@gmail.com>
2024-11-08 12:42:04 -0300

    refactor: CarFeaturesUpdateDto, CarInfoUpdateDto, CarUpdateDto and CarImagesNotFound

---
+++ src/main/java/com/ironhorse/dto/CarUpdateDto.java
@@ -0,0 +1,15 @@
+package com.ironhorse.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CarUpdateDto(
+        @NotNull @NotBlank String brand,
+        @NotNull @NotBlank String model,
+        @NotNull(message = "O ano de fabricação não pode ser nulo.")
+        @Min(value = 1900, message = "O ano de fabricação deve ser maior ou igual a 1900.")
+        Long manufactureYear,
+        @NotNull CarInfoDto carInfoUpdateDto
+) {
+}
