f943e27955a93c26452cb8fffc85effd530484a1
Evairlf <evairleitefeldmann@gmail.com>
2024-11-09 00:53:30 -0300

    refactor: CarOverviewService

--- src/main/java/com/ironhorse/service/CarOverviewService.java
+++ src/main/java/com/ironhorse/service/CarOverviewService.java
@@ -8,4 +8,7 @@ public interface CarOverviewService {
     CarOverviewResponseDto save(CarOverviewCreateDto carOverviewCreateDto, Long carId);
     CarOverviewListDto findByCarIdWithDetails(Long carId);
     CarOverviewResponseDto update(CarOverviewCreateDto carOverviewCreateDto, Long carId);
+    void setIsActive(Long carId, boolean isActive);
+    void setIsAvailable(Long carId, boolean isAvailable);
+    void increaseNumberOfTrips(Long carId);
 }
