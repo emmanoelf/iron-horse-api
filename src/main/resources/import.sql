2ebf1e566a2f83950df0dae20134dcde253d16b8
Evairlf <evairleitefeldmann@gmail.com>
2024-11-09 00:53:50 -0300

    refactor: import.sql added mocked data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,10 +5,15 @@ INSERT INTO user (created_at, updated_at, email, name, password, phone, role) VA
 -- Inserindo informações do usuário
 INSERT INTO user_info (created_at, updated_at, cpf, street_address, street_name, street_number, district, zipcode, city, state, latitude, longitude, driver_license, accept_comunication, is_terms_user, is_regularized, is_real_information, user_id) VALUES (NOW(), NOW(), '123.456.789-00', 'Rua A', 'Rua A', 123, 'Centro', '01000-000', 'São Paulo', 'SP', -23.5489, -46.6388, 'ABC123456', true, true, true, true, 1);
 
--- Inserindo dados em Car
-INSERT INTO car (created_at, updated_at, brand, model, manufacture_year, user_id) VALUES (NOW(), NOW(), 'Honda', 'Civic', 2021, 1);
-INSERT INTO car (created_at, updated_at, brand, model, manufacture_year, user_id) VALUES (NOW(), NOW(), 'Wolksvagen', 'Fusca', 2050, 2);
-INSERT INTO car (created_at, updated_at, brand, model, manufacture_year, user_id) VALUES (NOW(), NOW(), 'Ford', 'Focus', 2019, 2);
+-- Inserindo dados para o user 1
+INSERT INTO car (created_at, updated_at, brand, model, manufacture_year, user_id)VALUES (NOW(), NOW(), 'Honda', 'Civic', 2021, 1);
+
+INSERT INTO car (created_at, updated_at, brand, model, manufacture_year, user_id)VALUES (NOW(), NOW(), 'Ford', 'Fiesta', 2018, 1);
+
+-- Inserindo dados para o user 2
+INSERT INTO car (created_at, updated_at, brand, model, manufacture_year, user_id)VALUES (NOW(), NOW(), 'Volkswagen', 'Fusca', 1970, 2);
+
+INSERT INTO car (created_at, updated_at, brand, model, manufacture_year, user_id)VALUES (NOW(), NOW(), 'Chevrolet', 'Corsa', 2005, 2);
 
 
 -- Inserindo dados em car_info
@@ -21,4 +26,9 @@ INSERT INTO car_features (air_conditioner, alarm, anti_theft_secret, electric_wi
 INSERT INTO `car_overview` (is_active, is_available, number_trips, price, car_id, created_at, updated_at, description) VALUES ( 1, 1, 10, 150.00, 1, NOW(), NOW(), 'Carro em ótimo estado, pronto para viagens.'),(1, 0, 5, 200.50, 2, NOW(), NOW(), 'Carro disponível apenas para reservas antecipadas.');
 
 -- Inserindo em review
+
 INSERT INTO `review` (car_id, created_at, rate, updated_at, user_id, pros, cons) VALUES (1, NOW(), 4, NOW(), 1, 'Ótima experiência! O carro estava em excelentes condições.', 'Carro meio ruim mas tudo bem'), (1, NOW(), 5, NOW(), 2, 'Carro muito confortável e fácil de dirigir.', 'Carro fusca né mano sobe em morro azul'), (2, NOW(), 3, NOW(), 1, 'Bom carro, mas o consumo de combustível poderia ser melhor.', 'Só tava afim de reclamar mas gostei do carro'), (2, NOW(), 4, NOW(), 2, 'Serviço excelente, carro em ótimo estado.', 'Era melhor ter usado o carro do pelé');
+
+-- Inserindo em rental
+INSERT INTO `rental` (start_date, expected_end_date, real_end_date, status, user_id, car_id, created_at, updated_at) VALUES ('2024-10-08 01:10:00', '2024-10-09 23:10:00', NULL, 'ACTIVE', 1, 1, NOW(), NOW());
+
