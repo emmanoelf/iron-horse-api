138773896d176d9ad483a02957cb49d7eab6ac41
Emmanoel <emmanoel_95@hotmail.com>
2024-11-11 00:40:41 -0300

    refactor(StripeWebhookControllerImpl): change compare to paid

--- src/main/java/com/ironhorse/controller/impl/StripeWebhookControllerImpl.java
+++ src/main/java/com/ironhorse/controller/impl/StripeWebhookControllerImpl.java
@@ -31,27 +31,20 @@ public class StripeWebhookControllerImpl {
             Event event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
 
             if ("checkout.session.completed".equals(event.getType())) {
-                // Extrair a session
                 Session session = (Session) event.getData().getObject();
 
-                // Verificar o status do pagamento
-                if (Objects.equals(session.getPaymentStatus(), "PAID")) {
-                    // O pagamento foi confirmado, agora você pode salvar a locação no banco
-                    Long carId = Long.parseLong(session.getClientReferenceId()); // ou outro campo com ID do carro
-                    // Aqui você pode invocar um serviço para salvar a locação no banco
+                if (Objects.equals(session.getPaymentStatus(), "paid")) {
+                    Long carId = Long.parseLong(session.getClientReferenceId());
                     this.rentalServiceImpl.confirmRental(carId);
                 } else {
-                    // O pagamento não foi bem-sucedido
                     System.out.println("Pagamento não confirmado");
                 }
             }
 
             return ResponseEntity.ok("Webhook recebido com sucesso");
         } catch (SignatureVerificationException e) {
-            // Assinatura inválida
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Assinatura inválida");
         } catch (Exception e) {
-            // Outros erros
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno");
         }
     }
