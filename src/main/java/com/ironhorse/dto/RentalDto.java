0ff3b148e08447207e7d77154c2b046219b0bf2e
Emmanoel <emmanoel_95@hotmail.com>
2024-11-18 17:03:03 -0300

    refactor(RentalDto): add format to json

--- src/main/java/com/ironhorse/dto/RentalDto.java
+++ src/main/java/com/ironhorse/dto/RentalDto.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 public record RentalDto(
-        //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime startDate,
-        //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
         LocalDateTime expectedEndDate
 ) {
 }
