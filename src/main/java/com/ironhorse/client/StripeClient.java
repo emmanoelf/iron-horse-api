327020544a76cc3052a6776b84ed1fd8a46af333
emmanoelf <emmanoel_95@hotmail.com>
2025-06-14 15:28:50 -0300

    feat(StripeClient): add client to encapsule Stripe integration

---
+++ src/main/java/com/ironhorse/client/StripeClient.java
@@ -0,0 +1,13 @@
+package com.ironhorse.client;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StripeClient {
+    public Session createSession(SessionCreateParams params) throws StripeException {
+        return Session.create(params);
+    }
+}
