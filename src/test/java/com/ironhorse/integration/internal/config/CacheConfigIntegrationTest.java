f7554edff27a732aea36710c28d68beab6920502
emmanoelf <emmanoel_95@hotmail.com>
2025-10-09 11:14:38 -0300

    test(CacheConfigIntegrationTest): test caffeine cache manager

---
+++ src/test/java/com/ironhorse/integration/internal/config/CacheConfigIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.ironhorse.integration.internal.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Tag("integration")
+@Tag("internal")
+@ActiveProfiles("test")
+@SpringBootTest
+public class CacheConfigIntegrationTest {
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Test
+    @DisplayName("Should load caffeine cache manager")
+    public void shouldLoadCaffeineCacheManager(){
+        Cache cache = this.cacheManager.getCache("oneTimePassword");
+        String generatedOtp = "123456";
+        Long rentalId = 1L;
+
+        cache.put(generatedOtp, rentalId);
+        Long value = cache.get("123456", Long.class);
+
+        assertNotNull(cache);
+        assertEquals(rentalId, value);
+    }
+}
